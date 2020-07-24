49059f9f566c50c3bc3aa340a2927b308aef20a0
vanessabloes <vanessa.bloes@student.howest.be>
2020-07-25 01:42:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import useSound from 'use-sound';
+//import boopSfx from '../../sounds/boop.mp3';
 
 function App() {
+
+  const soundUrl = '/sounds/niels.mp3';
+
+  const [playbackRate, setPlaybackRate] = React.useState(1);
+
+  const [play] = useSound(soundUrl, {
+    playbackRate,
+    volume: 0.5,
+  });
+
+  const handleClick = () => {
+    setPlaybackRate(playbackRate);
+    play();
+  };
+
+  const [number, setNumber] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+
+          <form >
+            <label className="App-Form">
+              <p>Geef het juiste nummer in voor een happy ending:</p>
+              <input type="text" value={number} onChange={e => setNumber(e.target.value)} />
+            </label>
+          </form>
+          {number === "050880194" ?
+            <div>
+              <p>Duw op het hartje!</p>
+              <button className="App-heart" onClick={handleClick}>
+                <span role="img" aria-label="Heart">
+                  💖
+      </span>
+              </button>
+            </div>
+            :
+            <p></p>
+          }
+
+          {number.length === 9 && number !== "050880194" ?
+            <p>Probeer opnieuw!</p>
+            :
+            <p></p>
+          }
+
+
+        </header>
+
+      </div>
+
+    </>
   );
 }
 
